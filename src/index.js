e08bb6bec2f0e1f1f1bfe23587c4ee6a7d3a71a7
tuananh280199 <anhndt@reactplus.com>
2021-05-24 23:56:51 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require("cors");
-app.use(cors());
+// const cors = require("cors");
+// app.use(cors());
 //middleware
 app.use(
   express.urlencoded({
